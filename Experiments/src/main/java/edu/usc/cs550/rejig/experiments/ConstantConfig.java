ee8b098cf75453de8294be9b9dc15081c8b48edd
Likhit Dharmapuri <likhitd1992@gmail.com>
2018-12-06 16:05:06 -0800

    Adding YCSB and Experiment projects.

---
+++ Experiments/src/main/java/edu/usc/cs550/rejig/experiments/ConstantConfig.java
@@ -0,0 +1,11 @@
+package edu.usc.cs550.rejig.experiments;
+
+public class ConstantConfig {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
