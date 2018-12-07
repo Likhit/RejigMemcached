6e7501b410cabaf9bd8c87d3b1a9a69d84526a79
Likhit Dharmapuri <likhitd1992@gmail.com>
2018-12-06 16:44:38 -0800

    Adding simple experiment with a constant configuration.

--- Experiments/src/main/java/edu/usc/cs550/rejig/experiments/ConstantConfig.java
+++ Experiments/src/main/java/edu/usc/cs550/rejig/experiments/ConstantConfig.java
@@ -1,11 +1,60 @@
 package edu.usc.cs550.rejig.experiments;
 
-public class ConstantConfig {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+import edu.usc.cs550.rejig.interfaces.FragmentList;
+import edu.usc.cs550.rejig.interfaces.RejigConfig;
+import edu.usc.cs550.rejig.interfaces.RejigWriterGrpc;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+import java.util.concurrent.TimeUnit;
+
+public class ConstantConfig {
+  private ManagedChannel channel;
+
+  private RejigWriterGrpc.RejigWriterBlockingStub blockingStub;
+
+  public ConstantConfig(String host, int port) {
+    this(ManagedChannelBuilder.forAddress(host, port)
+      .usePlaintext()
+      .build()
+    );
+  }
+
+  ConstantConfig(ManagedChannel channel) {
+    this.channel = channel;
+    blockingStub = RejigWriterGrpc.newBlockingStub(channel);
+  }
+
+  public void shutDown() {
+    try {
+      if (channel != null) {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+        channel = null;
+      }
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
     }
+  }
+
+  public RejigConfig setConfig(FragmentList list) {
+    RejigConfig response;
+    try {
+      response = blockingStub.setConfig(list);
+    } catch (StatusRuntimeException e) {
+      throw new RuntimeException(e);
+    }
+    return response;
+  }
+
+  public static void main(String[] args) {
+    FragmentList ls = FragmentList.newBuilder()
+      .addAddress("localhost:11212")
+      .addAddress("localhost:11213")
+      .build();
+    ConstantConfig exp = new ConstantConfig("localhost", 50031);
+    RejigConfig response = exp.setConfig(ls);
+    System.out.println(response.toString());
+  }
 }
